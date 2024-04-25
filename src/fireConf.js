1f1ab910080a66e5b62b305d81f0ec9c8e222502
Mohamed Khashana <118824280+mokhashana@users.noreply.github.com>
2024-04-26 01:01:35 +0300

    Update fireConf.js

--- src/fireConf.js
+++ src/fireConf.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCofnpVXr30EqsN9YDcF8mhYzrd2KKdXpc",
-    authDomain: "advcrud-eedff.firebaseapp.com",
-    projectId: "advcrud-eedff",
-    storageBucket: "advcrud-eedff.appspot.com",
-    messagingSenderId: "871276924076",
-    appId: "1:871276924076:web:1c86f0b07f5a21020f2309"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
